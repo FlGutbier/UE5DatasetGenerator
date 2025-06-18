4a5672b23bb12400be3bca2e21a3af10628ebd3c
FlGutbier <160612144+FlGutbier@users.noreply.github.com>
2025-06-18 15:38:30 +0200

    Update DatasetRendererBachelorThesis.h

--- Plugin/Source/DatasetRendererBachelorThesis/Public/DatasetRendererBachelorThesis.h
+++ Plugin/Source/DatasetRendererBachelorThesis/Public/DatasetRendererBachelorThesis.h
@@ -1,4 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright (c) 2025 Florian Gutbier
+// 
+// This source code is part of the UE5 Plugin developed for the Bachelor's thesis
+// at the University of Bamberg.
+// 
+// Released under the MIT License. See LICENSE file for details.
 
 #pragma once
 
